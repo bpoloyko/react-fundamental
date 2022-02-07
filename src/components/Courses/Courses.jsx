34356db1c6e8f913d5197767898af79a9146ac41
Boris Poloyko <boris_poloyko@epam.com>
2022-02-07 15:40:37 +0300

    Chnage search component

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -12,11 +12,6 @@ const Courses = ({ onAddNewCourseClick, courses, authors }) => {
 	const [searchString, setSearchString] = useState('');
 
 	const searchCourse = (searchString) => {
-		if (searchString === '') {
-			setFoundCourses(courses);
-			return;
-		}
-
 		let foundCourses = courses.filter(
 			(course) =>
 				course.title.toLowerCase().includes(searchString.toLowerCase()) ||
@@ -33,7 +28,12 @@ const Courses = ({ onAddNewCourseClick, courses, authors }) => {
 						buttonText='Search'
 						placeholderText='Enter course name or id...'
 						onClick={() => searchCourse(searchString)}
-						onChange={(e) => setSearchString(e.target.value)}
+						onChange={(e) => {
+							setSearchString(e.target.value);
+							if (e.target.value === '') {
+								setFoundCourses(courses);
+							}
+						}}
 					/>
 				</div>
 				<div className='add-course-button'>
