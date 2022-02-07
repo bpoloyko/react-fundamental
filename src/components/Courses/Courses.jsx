6a488c4cfb367073df90ea2bed716897dc045d01
Boris Poloyko <boris_poloyko@epam.com>
2022-02-07 15:23:01 +0300

    First hometask

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -12,6 +12,11 @@ const Courses = ({ onAddNewCourseClick, courses, authors }) => {
 	const [searchString, setSearchString] = useState('');
 
 	const searchCourse = (searchString) => {
+		if (searchString === '') {
+			setFoundCourses(courses);
+			return;
+		}
+
 		let foundCourses = courses.filter(
 			(course) =>
 				course.title.toLowerCase().includes(searchString.toLowerCase()) ||
@@ -28,12 +33,7 @@ const Courses = ({ onAddNewCourseClick, courses, authors }) => {
 						buttonText='Search'
 						placeholderText='Enter course name or id...'
 						onClick={() => searchCourse(searchString)}
-						onChange={(e) => {
-							setSearchString(e.target.value);
-							if (e.target.value === '') {
-								setFoundCourses(courses);
-							}
-						}}
+						onChange={(e) => setSearchString(e.target.value)}
 					/>
 				</div>
 				<div className='add-course-button'>
